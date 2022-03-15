6bcc9023ae91aa6f6c87f6ff7b6c9b58daecf7b4
Luigi_Capello <Stepa.kashin.2018@gmail.com>
2022-03-15 14:30:04 +0300

    Borisov lessin 15mar

---
+++ class_work3/class_work3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_work3
+{
+    public partial class Form1 : Form
+    {
+        private stripe _mystripe;
+        private Random _genran;
+        private const int _StripeWidth = 100, _StripeHight = 25;
+        List<stripe> All_Stripes;
+        public Form1()
+        {
+            InitializeComponent();
+
+            All_Stripes = new List<stripe>();
+            _genran = new Random();
+            int x = _genran.Next(this.ClientSize.Width - _StripeWidth);
+            int y = _genran.Next(this.ClientSize.Height - _StripeHight);
+
+            AddStripes(10);
+            timer1.Start();
+            //_mystripe = new stripe(x, y, _StripeWidth, _StripeHight);
+
+            //All_Stripes.Add(_mystripe);
+        }
+
+        private void AddStripes(int v)
+        {
+            while (v > 0)
+            {
+                int x = _genran.Next(this.ClientSize.Width - _StripeWidth);
+                int y = _genran.Next(this.ClientSize.Height - _StripeHight);
+                _mystripe = new stripe(x, y, _StripeWidth, _StripeHight);
+                int r = _genran.Next(256);
+                int g = _genran.Next(256);
+                int b = _genran.Next(256);
+
+                _mystripe.SetColor(Color.FromArgb(r, g, b));
+
+                All_Stripes.Add(_mystripe);
+                v--;
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            for (int i = All_Stripes.Count-1; i >= 0 ; i--) 
+            {
+                if (All_Stripes[i].Inside(e.X, e.Y)) 
+                {
+                    bool key = true;
+
+                    for (int j = i + 1; j < All_Stripes.Count; j++) 
+                    {
+                        if (All_Stripes[j].Intersect(All_Stripes[i])) 
+                        {
+                            key = false;
+                            break; 
+                        }
+                    }
+                    if (key) 
+                    {
+                        All_Stripes.RemoveAt(i);
+                        Invalidate();
+                    }
+                    
+                    break;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AddStripes(1);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            foreach(stripe x in All_Stripes) x.Draw(g);
+            
+        }
+    }
+}
---
+++ class_work3/class_work3/stripe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace class_work3
+{
+    class stripe
+    {
+
+        private Rectangle _rect;
+        Color _color;
+
+        public stripe(int x,int y, int width,int hight) 
+        {
+            _rect = new Rectangle(x, y, width, hight);
+            _color = Color.Aqua;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(_color), _rect);
+            g.DrawRectangle(new Pen(Color.Black), _rect);
+        }
+
+        public bool Inside(int x, int y)
+        {
+            if (x < _rect.Left || x > _rect.Right) return false;
+            if (y < _rect.Top || y > _rect.Bottom) return false;
+
+            return true;
+        }
+
+        public bool Intersect(stripe stripe)
+        {
+            if (_rect.IntersectsWith(stripe._rect)) 
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        internal void SetColor(Color color)
+        {
+            _color = color;
+        }
+    }
+}
